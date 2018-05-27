1db3601f4712910dd23386d46db0405e56b6cb8c
laurent bernabé <Laurent_Bernabe_student2018@wilder.school>
2018-05-27 22:28:50 +0200

    Adding borders inside the game area.

--- source/view/AppMainWindow.d
+++ source/view/AppMainWindow.d
@@ -7,10 +7,14 @@ import gtk.Widget;
 
 export class AppMainWindow: MainWindow
 {
+	private const int BORDER_DIMENSION = 10;
+	private const auto BORDER_COLOR = [200, 0, 120];
+
+
 	this(const string title)
 	{
 		super(title);
-		setDefaultSize(200, 200);
+		setDefaultSize(201, 201);
 		setResizable(false);
 		setIconFromFile("tic-tac-toe.png");
 
@@ -21,14 +25,38 @@ export class AppMainWindow: MainWindow
 
 	void initializeChildren()
 	{
-		DrawingArea canvas = new DrawingArea(200, 200);
-		canvas.addOnDraw((Scoped!(Context) context, Widget widget) {
-			context.rectangle(0, 0, getAllocatedWidth(), getAllocatedHeight());
-			context.setSourceRgb(255, 0, 0);
-			context.fill();
-
+		DrawingArea canvas = new DrawingArea(201, 201);
+		canvas.addOnDraw((Scoped!(Context) context, Widget widget)
+		{
+			foreach (const borderBigX; 1..3)
+			{
+				drawHorizontalBorder(context, borderBigX);
+				drawVerticalBorder(context, borderBigX);
+			}
+			
 			return true;
 		});
 		add(canvas);
 	}
+
+	void drawHorizontalBorder(Context context, const int borderBigX)
+	{
+		const int cellsSize = getAllocatedWidth() / 3;
+		const int borderX = cellsSize * borderBigX - BORDER_DIMENSION/2;
+
+		context.rectangle(borderX, 0, BORDER_DIMENSION, getAllocatedHeight());
+		context.setSourceRgb(BORDER_COLOR[0], BORDER_COLOR[1], BORDER_COLOR[2]);
+		context.fill();
+	}
+
+	void drawVerticalBorder(Context context, const int borderBigX)
+	{
+		const int cellsSize = getAllocatedWidth() / 3;
+		const int borderY = cellsSize * borderBigX - BORDER_DIMENSION/2;
+
+		context.rectangle(0, borderY, getAllocatedWidth(), BORDER_DIMENSION);
+		context.setSourceRgb(BORDER_COLOR[0], BORDER_COLOR[1], BORDER_COLOR[2]);
+		context.fill();
+	}
+
 }
